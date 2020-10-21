40e806a6fa19cf1c5871e1797c669a49f91d255c
George Benjamin-Schonberger <george.benjamin@gmail.com>
2020-10-21 12:22:02 +0300

    Version 1.0.0-beta1

---
+++ src/CultureHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+
+namespace Netopes.Localization
+{
+    public class CultureHandler
+    {
+        public void Set(HttpContext httpContext, string culture)
+        {
+            httpContext.Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)));
+        }
+
+        public async Task SetAsync(HttpContext httpContext, string culture)
+        {
+            await Task.Run(() => Set(httpContext, culture));
+        }
+    }
+}
